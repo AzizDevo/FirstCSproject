a4b25889eb57da93cea95a1c04397ad7f48631c6
AzizDevo <125346236+AzizDevo@users.noreply.github.com>
2023-02-26 23:05:12 +0400

    FirstCsprogram

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//#region Task-1
+
+//Console.WriteLine("Please, enter your test score:");
+//int Score =int.Parse(Console.ReadLine());
+
+//    if (Score <= 100 && Score>=91)
+//    {
+//        Console.WriteLine("A");
+//    }
+//    else if (Score <=90 && Score>=81)
+//    {
+//        Console.WriteLine("B"); 
+//    }
+//    else if (Score <=80 && Score >= 71)
+//    {
+//        Console.WriteLine("C");
+//    }
+//    else if(Score <=70 && Score >= 61)
+//    {
+//        Console.WriteLine("D");
+//    }
+//    else if (Score <=60 && Score >= 51)
+//    {
+//        Console.WriteLine("E");
+//    }
+//    else if (Score < 51)
+//    {
+//        Console.WriteLine("Unfortuately you failed the exam");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Please, enter a valid score.");
+//    }
+
+//#endregion
+
+
+//#region Task-2
+
+//int[] arr = {-6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6};
+
+//int PositiveNumbers = 0;
+//int NegativeNumbers = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] > 0)
+//    {
+//        PositiveNumbers++;
+//    }
+//    else if (arr[i] < 0)
+//    {
+//        NegativeNumbers++;
+//    }
+//}
+
+//Console.WriteLine("Here are the positive numbers: " + PositiveNumbers);
+//Console.WriteLine("Here are the negative numbers: " + NegativeNumbers);
+
+//#endregion 
+
+
+//#region Task-3
+
+//int[] arr = { 575, 656, 493, 908, 103, 49, 161 };
+
+//int Biggest = 0;
+
+//for (int i = 0; i<arr.Length; i++)
+//{
+//    if (arr[i] > Biggest)
+//    {
+//        Biggest = arr[i];   
+//    }
+//}
+
+//Console.WriteLine(Biggest + " is the biggest number");
+
+//#endregion 
+
